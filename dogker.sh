d6a690e1f1707b10d08ba64f41d371f9bf2a942e
Allen Dai <130431901+daiallen07@users.noreply.github.com>
2024-10-04 14:04:54 -0700

    added some notes

--- dogker.sh
+++ dogker.sh
@@ -3,3 +3,7 @@ sudo apt install docker.io -y
 sudo apt install docker-compose -y
 docker pull docker.io/kalilinux/kali-rolling
 docker run --tty --interactive kalilinux/kali-rolling
+
+
+#after you get into the docker terminal, run apt update
+#then run the script
