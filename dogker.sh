c1075ffaeee69e45c784a2fb7f71cfc97881afd1
Allen Dai <130431901+daiallen07@users.noreply.github.com>
2024-10-05 15:27:33 -0700

    added fix (maybe)

--- dogker.sh
+++ dogker.sh
@@ -7,3 +7,5 @@ sudo docker run --tty --interactive kalilinux/kali-rolling
 
 #after you get into the docker terminal, run apt update
 #then run the script
+#echo "deb http://http.kali.org/kali kali-last-snapshot main contrib non-free" | tee /etc/apt/sources.list
+#maybe fixes apt update issue
