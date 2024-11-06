2bc1f671517c2e4287c847bec4219f4fff52cdd0
Allen Dai <130431901+daiallen07@users.noreply.github.com>
2024-11-06 14:17:42 -0800

    Update nmap_automation.sh

--- nmap_automation.sh
+++ nmap_automation.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+#get ip from ping sweep
+nmap -sn -iL iplist.txt -oG foundIp.txt; grep -v Down foundIp.txt; grep -oP '\b(?:\d{1,3}\.){3}\d{1,3}\b' foundIp.txt
+echo "Ping sweep complete"
 
+#use ip from previous output and conduct basic nmap scan with xml and nmap format output
+nmap -sC -sV -iL foundIp.txt -p- -Pn -oN nmap_output -oX xml_ouput
+echo "Nmap complete"
 
-
-# Extract IP addresses using regular expression
-grep -oP '\b(?:\d{1,3}\.){3}\d{1,3}\b' ""
+#convert xml into HTML
+xsltproc scan.xml > scan.html
+echo "html file created"
